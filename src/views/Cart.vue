35141b75259171d662f31cb3c61d4d5cc9fa1b30
Na-aimOverlord <91248779+Na-aim@users.noreply.github.com>
2022-03-25 23:55:39 +0200

    update cart

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -3,6 +3,9 @@
   <div class="text-effect">
     <span>in production</span>
 </div>
+<div class="container">
+  <img src="https://i.postimg.cc/8PBgrWWn/jujutsu-kaisen.jpg" alt="" style="width:100%;">
+</div>
 </template>
 
 <script>
@@ -14,5 +17,21 @@ export default {
 <style>
 .somesome{
     padding-top: 50px;
+}
+.container {
+  position: relative;
+  max-width: auto;
+  margin: 0 auto;
+}
+.container img {vertical-align: middle;}
+
+.container .content {
+  position: absolute;
+  bottom: 0;
+  background: rgb(0, 0, 0); /* Fallback color */
+  background: rgba(0, 0, 0, 0.3); /* Black background with 0.5 opacity */
+  color: #f1f1f1;
+  width: 99%;
+  padding: 7px;
 }
 </style>
